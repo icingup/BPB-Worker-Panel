f01dc42a313af0c1ebf1702c58a23358695e6c0f
goadmin <icingup@gmail.com>
2024-11-07 20:51:52 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -6,7 +6,7 @@ import { SignJWT, jwtVerify } from 'jose';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'd3da750c-fe02-4709-8bca-a9d01757e18c';
 let trojanPassword = `bpb-trojan`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
